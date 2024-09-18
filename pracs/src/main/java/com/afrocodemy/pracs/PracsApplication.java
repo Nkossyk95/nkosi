18777539f58415ba7b47dce5d529a3ae117038fc
Nkossyk95 <95701026+Nkossyk95@users.noreply.github.com>
2024-09-18 14:32:50 +0200

    Add files via upload

---
+++ pracs/src/main/java/com/afrocodemy/pracs/PracsApplication.java
@@ -0,0 +1,13 @@
+package com.afrocodemy.pracs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracsApplication.class, args);
+	}
+
+}
---
+++ pracs/src/test/java/com/afrocodemy/pracs/PracsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afrocodemy.pracs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
